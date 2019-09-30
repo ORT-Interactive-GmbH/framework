77143504fd7cac57f31ce3af9417fba7092b45d5
Manuel Stommel <manuel.stommel@ort-online.net>
2019-09-30 15:15:20 +0200

    patched Redirector to inject UrlGenerator interface instead of concrete implementation

--- src/Illuminate/Routing/Redirector.php
+++ src/Illuminate/Routing/Redirector.php
@@ -5,6 +5,7 @@ namespace Illuminate\Routing;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Traits\Macroable;
 use Illuminate\Session\Store as SessionStore;
+use Illuminate\Contracts\Routing\UrlGenerator as UrlGeneratorContract;
 
 class Redirector
 {
@@ -13,7 +14,7 @@ class Redirector
     /**
      * The URL generator instance.
      *
-     * @var \Illuminate\Routing\UrlGenerator
+     * @var UrlGeneratorContract
      */
     protected $generator;
 
@@ -27,10 +28,10 @@ class Redirector
     /**
      * Create a new Redirector instance.
      *
-     * @param  \Illuminate\Routing\UrlGenerator  $generator
+     * @param  UrlGeneratorContract  $generator
      * @return void
      */
-    public function __construct(UrlGenerator $generator)
+    public function __construct(UrlGeneratorContract $generator)
     {
         $this->generator = $generator;
     }
@@ -212,7 +213,7 @@ class Redirector
     /**
      * Get the URL generator instance.
      *
-     * @return \Illuminate\Routing\UrlGenerator
+     * @return UrlGeneratorContract
      */
     public function getUrlGenerator()
     {
